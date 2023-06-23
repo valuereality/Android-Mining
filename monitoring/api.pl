4f1f7c86f572eba91ae773e9514449aa29d53d86
Dean Conway <dean.conway@valuereality.com.au>
2023-06-23 18:58:12 +1000

    change config and ip address ranges

--- monitoring/api.pl
+++ monitoring/api.pl
@@ -3,15 +3,25 @@
 use strict;
 use warnings;
 use Getopt::Long qw(GetOptions);
+
+# default values
 my $command = "summary";
 my $address = "127.0.0.1";
-my $port = "4086";
+my $port = "4068";
+
+# get command line options
 GetOptions('cmd=s' => \$command,
            'address=s' => \$address,
            'port=s' => \$port,
 ) or die "Usage: $0 --cmd COMMAND --address ADDRESS --port PORT\n";
+
+# create socket
 use Socket;
+
+# create socket
 use IO::Socket::INET;
+
+# auto-flush on socket
 my $sock = new IO::Socket::INET (
     PeerAddr => $address,
     PeerPort => $port,
@@ -19,6 +29,9 @@ my $sock = new IO::Socket::INET (
     ReuseAddr => 1,
     Timeout => 2,
 );
+
+
+# send command
 if ($sock) {
     print $sock $command;
     my $res = "";
